fc43ec2ad0f42d03772e43c745a0396c04545e9e
kbugayenko <k.bugayenko@gmail.com>
2022-11-20 00:13:56 +0200

    update 01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+let interval = null;
+
+function onStart() {
+  refs.btnStart.disabled = true;
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  refs.btnStart.disabled = false;
+  clearInterval(interval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
