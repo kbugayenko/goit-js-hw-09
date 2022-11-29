bb59c1b8afc1d246637c72f2aade740112d3a2bc
kbugayenko <k.bugayenko@gmail.com>
2022-11-30 00:41:43 +0200

    last version 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,7 +11,9 @@ const refs = {
   seconds: document.querySelector('span[data-seconds]'),
 };
 
-// refs.input.addEventListener('click', flatpickr);
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 refs.btnStart.disabled = true;
 
@@ -21,32 +23,40 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
     if (selectedDates[0] - new Date() < 0) {
       Notiflix.Notify.failure('Please choose a date in the future');
-      return;
+    } else {
+      refs.btnStart.disabled = false;
+      refs.btnStart.addEventListener('click', () => {
+        let timerId = setInterval(() => {
+          const ms = selectedDates[0] - new Date();
+          convertMs(ms);
+          function convertMs(ms) {
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+
+            const days = Math.floor(ms / day);
+            const hours = Math.floor((ms % day) / hour);
+
+            const minutes = Math.floor(((ms % day) % hour) / minute);
+
+            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+            addLeadingZero();
+            refs.days.textContent = addLeadingZero(`${days}`);
+            refs.hours.textContent = addLeadingZero(`${hours}`);
+            refs.minutes.textContent = addLeadingZero(`${minutes}`);
+            refs.seconds.textContent = addLeadingZero(`${seconds}`);
+          }
+          if (ms < 1000) {
+            clearInterval(timerId);
+          }
+        }, 1000);
+      });
     }
-    refs.btnStart.addEventListener('click');
   },
 };
 
 flatpickr(refs.input, options);
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
